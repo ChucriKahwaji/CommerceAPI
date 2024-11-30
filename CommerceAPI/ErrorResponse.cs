16b390b8181c7d04ec810dfd8587b788ff31e71b
Chucri kahwaji <chucrikahwaji@gmail.com>
2024-11-29 23:19:42 -0500

    added environment-specific error handling

---
+++ CommerceAPI/ErrorResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace API
+{
+    public class ErrorResponse
+    {
+        public string CorrelationId { get; set; }
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string StackTrace { get; set; } // Optional, only included in Development
+    }
+}
