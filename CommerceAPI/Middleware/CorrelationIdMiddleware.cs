71473803165149ebd8eae3b8ac0d5d8c2d7a6015
Chucri kahwaji <chucrikahwaji@gmail.com>
2024-11-29 19:01:47 -0500

    added correlation id to logs

---
+++ CommerceAPI/Middleware/CorrelationIdMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using API;
+using log4net;
+
+namespace CommerceAPI.Middleware
+{
+    public class CorrelationIdMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CorrelationIdMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext, ILogger<CorrelationIdMiddleware> logger)
+        {
+            // Check for an existing correlation ID in the request
+            var correlationId = httpContext.Request.Headers["X-Correlation-ID"].FirstOrDefault() ?? Guid.NewGuid().ToString();
+
+            // Set the correlation ID in HttpContext for use in the application
+            httpContext.Items["CorrelationId"] = correlationId;
+            LogicalThreadContext.Properties["CorrelationId"] = correlationId;
+
+            // Use logging scope to include the correlation ID in all log entries
+            using (logger.BeginScope(new Dictionary<string, object> { ["CorrelationId"] = correlationId }))
+            {
+                await _next(httpContext);
+            }
+        }
+    }
+}
